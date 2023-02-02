85e13885d87a8cf23ab0cdb1e115e407e4d0388c
Alex JL <alexsrk09@gmail.com>
2023-02-02 10:00:02 +0100

    all first files uploades

---
+++ src/components/home/HomePhoto/HomePhoto.jsx
@@ -0,0 +1,11 @@
+import { StyledImage, HomePhotoWrapper } from "./HomePhoto.styled";
+
+const HomePhoto = () => {
+  return (
+    <HomePhotoWrapper>
+      <StyledImage src="https://avatars.githubusercontent.com/u/78103379?v=4" alt="home photo" />
+    </HomePhotoWrapper>
+  );
+};
+
+export default HomePhoto;
---
+++ src/components/home/HomeTags/HomeTags.styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const HomeTagsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TagsContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 50px;
+  gap: 8px;
+`;
+
+export const Tag = styled.div`
+  border: 1px solid #000;
+  border-radius: 5px;
+  width: fit-content;
+  padding: 5px;
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: rotate(5deg);
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
